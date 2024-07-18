7c6a9fd5930dcf196df7cb71fd8855d4cd003018
amorajoaovictor <amorajoaovictor2@gmail.com>
2024-07-18 16:52:58 -0300

    chore: Add link app and models for URL shortening feature

--- encurta_url/link/admin.py
+++ encurta_url/link/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Link    
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ( 'url',"urlEncurtada")
+
+
+admin.site.register(Link, LinkAdmin)
--- encurta_url/link/models.py
+++ encurta_url/link/models.py
@@ -1,9 +1,21 @@
+import hashlib
+import random
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.contrib import admin
+from shortuuid import ShortUUID
 # Create your models here.
 class Link(models.Model):
 	url = models.URLField()
 	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	urlEncurtada = models.URLField(editable=False, auto_created=True, blank=True,)
+	def save(self, *args, **kwargs):
+		unique_id = ShortUUID().random(length=random.randint(5, 10))
+		self.urlEncurtada = "https://encurtador/"+str(unique_id)
+		super(Link, self).save(*args, **kwargs)
 
+	
 	def __str__(self):
-		return f'{self.url} - {self.hash}'
+		return f"{str(self.url)} - {str(self.urlEncurtada)}"
+
